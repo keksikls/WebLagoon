f8d93058f22be56290a8065d88e48e027eeb52d0
Киравич Курова <jantsev.ruslan@yandex.ru>
2025-02-24 02:59:10 +0300

    Add project files.

---
+++ WebLagoon.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebLagoon.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebLagoon.Web/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
