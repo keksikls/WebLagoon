b4ffa6f832983b5d97a12ea2ea9af957f0149dc3
Киравич Курова <jantsev.ruslan@yandex.ru>
2025-02-25 07:39:19 +0300

    changes

--- WebLagoon.Domain/Entities/Villa.cs
+++ WebLagoon.Domain/Entities/Villa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace WebLagoon.Domain.Entities
 {
     public class Villa
     {
+        [Key]
         public int Id { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
@@ -15,7 +17,7 @@ namespace WebLagoon.Domain.Entities
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
         public string? ImageUrl { get; set; }
-        public DateTime? CreatedDate { get; set; }
+        public DateTime? CreatedDate { get; set; } = DateTime.UtcNow;
         public DateTime? UpdatedDate { get;set; }
     }
 }
